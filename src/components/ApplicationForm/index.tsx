b100cf9fd36bfca1528413a522a371c1e521871e
L I L V C <simidawalang@gmail.com>
2023-10-01 22:17:18 +0100

    removed function causing failure

--- src/components/ApplicationForm/index.tsx
+++ src/components/ApplicationForm/index.tsx
@@ -4,115 +4,102 @@ import { fetchForm, submitForm } from "../../helpers";
 import { ImCross } from "react-icons/im";
 
 const ApplicationForm = () => {
-  const [formData, setFormData] = useState<any>({});
+  const [formData, setFormData] = useState<any>({
+    data: {
+      id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+      type: "applicationForm",
+      attributes: {
+        coverImage: "http://example.com",
+        personalInformation: {
+          firstName: {
+            internalUse: false,
+            show: true,
+          },
+          lastName: {
+            internalUse: false,
+            show: true,
+          },
+          emailId: {
+            internalUse: false,
+            show: true,
+          },
+          phoneNumber: {
+            internalUse: false,
+            show: true,
+          },
+          nationality: {
+            internalUse: false,
+            show: true,
+          },
+          currentResidence: {
+            internalUse: false,
+            show: true,
+          },
+          idNumber: {
+            internalUse: false,
+            show: true,
+          },
+          dateOfBirth: {
+            internalUse: false,
+            show: true,
+          },
+          gender: {
+            internalUse: false,
+            show: true,
+          },
+          personalQuestions: [
+            {
+              id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+              type: "Paragraph",
+              question: "string",
+              choices: ["string"],
+              maxChoice: 0,
+              disqualify: false,
+              other: false,
+            },
+          ],
+        },
+        profile: {
+          education: {
+            mandatory: true,
+            show: true,
+          },
+          experience: {
+            mandatory: true,
+            show: true,
+          },
+          resume: {
+            mandatory: true,
+            show: true,
+          },
+          profileQuestions: [
+            {
+              id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+              type: "Paragraph",
+              question: "string",
+              choices: ["string"],
+              maxChoice: 0,
+              disqualify: false,
+              other: false,
+            },
+          ],
+        },
+        customisedQuestions: [
+          {
+            id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+            type: "Paragraph",
+            question: "string",
+            choices: ["string"],
+            maxChoice: 0,
+            disqualify: false,
+            other: false,
+          },
+        ],
+      },
+    },
+  });
 
-  const [coverImage, setCoverImage] = useState("");
-
-  // {
-  //   data: {
-  //     id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
-  //     type: "applicationForm",
-  //     attributes: {
-  //       coverImage: "http://example.com",
-  //       personalInformation: {
-  //         firstName: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         lastName: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         emailId: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         phoneNumber: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         nationality: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         currentResidence: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         idNumber: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         dateOfBirth: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         gender: {
-  //           internalUse: false,
-  //           show: true,
-  //         },
-  //         personalQuestions: [
-  //           {
-  //             id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
-  //             type: "Paragraph",
-  //             question: "string",
-  //             choices: ["string"],
-  //             maxChoice: 0,
-  //             disqualify: false,
-  //             other: false,
-  //           },
-  //         ],
-  //       },
-  //       profile: {
-  //         education: {
-  //           mandatory: true,
-  //           show: true,
-  //         },
-  //         experience: {
-  //           mandatory: true,
-  //           show: true,
-  //         },
-  //         resume: {
-  //           mandatory: true,
-  //           show: true,
-  //         },
-  //         profileQuestions: [
-  //           {
-  //             id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
-  //             type: "Paragraph",
-  //             question: "string",
-  //             choices: ["string"],
-  //             maxChoice: 0,
-  //             disqualify: false,
-  //             other: false,
-  //           },
-  //         ],
-  //       },
-  //       customisedQuestions: [
-  //         {
-  //           id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
-  //           type: "Paragraph",
-  //           question: "string",
-  //           choices: ["string"],
-  //           maxChoice: 0,
-  //           disqualify: false,
-  //           other: false,
-  //         },
-  //       ],
-  //     },
-  //   },
-  // }
-
-  const fetchData = async () => {
-    const res = await fetchForm();
-    if (res?.data) {
-      setFormData(res.data);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [coverImage, setCoverImage] = useState<any>("");
 
   const submitData = async () => {
     const res = await submitForm(formData);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,11 @@
 export { default as ApplicationForm } from "./ApplicationForm";
 export { default as Layout } from "./Layout";
+
+
+
+  // const fetchData = async () => {
+  //   const res = await fetchForm();
+  //   if (res?.data) {
+  //     setFormData(res.data);
+  //   }
+  // }
